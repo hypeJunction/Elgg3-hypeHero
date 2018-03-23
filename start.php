21ab9951a50a17d40ae8d338115a499297b150f5
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2018-03-23 17:29:19 +0100

    fix(menus): set higher priorities on cover menus

--- start.php
+++ start.php
@@ -11,9 +11,9 @@ return function () {
 		elgg_register_plugin_hook_handler('entity:cover:sizes', 'all', \hypeJunction\Hero\DefineCoverSizes::class);
 		elgg_register_plugin_hook_handler('entity:cover:url', 'all', \hypeJunction\Hero\SetCoverArtwork::class);
 
-		elgg_register_plugin_hook_handler('register', 'menu:hero', \hypeJunction\Hero\HeroMenu::class);
-		elgg_register_plugin_hook_handler('register', 'menu:cover', \hypeJunction\Hero\CoverMenu::class);
-		elgg_register_plugin_hook_handler('register', 'menu:actions', \hypeJunction\Hero\ActionsMenu::class);
+		elgg_register_plugin_hook_handler('register', 'menu:hero', \hypeJunction\Hero\HeroMenu::class, 900);
+		elgg_register_plugin_hook_handler('register', 'menu:cover', \hypeJunction\Hero\CoverMenu::class, 900);
+		elgg_register_plugin_hook_handler('register', 'menu:actions', \hypeJunction\Hero\ActionsMenu::class, 900);
 
 		elgg_unregister_plugin_hook_handler('register', 'menu:title', '_groups_title_menu');
 		elgg_register_plugin_hook_handler('register', 'menu:actions', '_groups_title_menu', 400);
